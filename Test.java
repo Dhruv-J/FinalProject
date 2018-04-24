83f6f722dafe81010022a137e736d970ebcf126a
Dhruv <dhruvjain1@dhruvs-air.esuhsd.org>
2018-04-24 09:04:39 -0700

    Commit with Circle/Ovali#

--- Test.java
+++ Test.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.util.*;
 import javax.swing.JFrame;
@@ -5,20 +6,30 @@ import javax.swing.JFrame;
 public class Test extends JFrame {
 
     public Test() {
-        setTitle("Test");
-        setSize(960, 960);
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Test");                           //set name of the window
+        setSize(960, 960);                          //set window size
+        setVisible(true);                           //set it so it is visible
+        setDefaultCloseOperation(EXIT_ON_CLOSE);    //set it so that the program exits when x button is pressed
 
     }
+    /*
+    public void paint(Graphics g) {
+        g.setColor(Color.RED);                      //set color to red
+        g.drawRect(480, 480, 200, 100);             //draw a rectangle at 480, 480 with size 200 x 100
+        g.setColor(Color.BLUE);                     //set color to blue
+        g.fillRect(240, 240, 200, 100);             //draw and fill a rectangle at 240, 240 with size 200 x 100
+    }
+    */
 
     public void paint(Graphics g) {
-        g.drawRect(480, 480, 200, 100);
-        g.fillRect(240, 240, 200, 100);
+        g.setColor(Color.GREEN);                    //set color to green
+        g.drawOval(480, 480, 200, 200);             //draw a circle at 480, 480 with radius 100
+        g.setColor(Color.YELLOW);                   //set color to yellow
+        g.fillOval(240, 240, 200, 200);             //fill a circle at 240, 240 with radius 100    
     }
 
     public static void main(String[] args) {
-        Test t = new Test();
-        t.paint(null);
+        Test t = new Test();                        //declaring Test object
+        t.paint(null);                              //calling paintRectangle method
     }
 }
