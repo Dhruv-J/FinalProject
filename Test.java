f3f70f92f1be928e3f791b156b70ff8801a91cf4
Dhruv <dhruvjain1@Dhruvs-MacBook-Air.local>
2018-04-23 18:19:07 -0700

    This is the first commit being made#

---
+++ Test.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import javax.swing.JFrame;
+
+public class Test extends JFrame {
+
+    public Test() {
+        setTitle("Test");
+        setSize(400, 400);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+    public static void main(String[] args) {
+        Test t = new Test();
+    }
+}
